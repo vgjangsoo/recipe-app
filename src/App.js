efb2fbf1f3562ff95377e148a05e987826aeb66b
Sungil Saul Lee <saul@Sungils-MacBook.local>
2020-01-25 22:29:33 -0800

    ingredients showing

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
         </button>
       </form>
       {recipes.map(recipe => (
-        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
       ))}
     </div>
   );
